5de0b2d26df8b11c241415f53c760227eaf7eea8
Zehra <95143820+aydinfz@users.noreply.github.com>
2023-07-15 10:50:08 +0300

    hash password

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -16,8 +17,10 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre("save", function (next) {
+userSchema.pre("save", async function (next) {
   console.log("user about to be created & saved", this);
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt)
   next();
 });
 
