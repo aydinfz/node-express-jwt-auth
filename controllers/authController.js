7e634ce65a0e07279b2d4e89db37342fbb1dd00e
Zehra <95143820+aydinfz@users.noreply.github.com>
2023-07-15 09:55:56 +0300

    create mongoose validation and error message

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,38 @@
-const { request } = require("express");
+const User = require("../models/User");
+
+const handleErrors = (err) => {
+  //console.log(err.message, err.code);
+  let errors = { email: "", password: "" };
+
+  if (err.code === 11000) {
+    errors.email = "That email is already exist";
+    return errors;
+  }
+
+  if (err.message.includes("user validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+  //console.log("errrrr", errors);
+  return errors;
+};
 
 module.exports.signup_get = (req, res) => {
   res.render("signup");
 };
-module.exports.signup_post = (req, res) => {
+module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
-  res.send("new signup");
+  try {
+    const user = await User.create({
+      email,
+      password,
+    });
+    res.status(201).json(user);
+  } catch (err) {
+    const error = handleErrors(err);
+    res.status(400).json({ error });
+  }
 };
 module.exports.login_get = (req, res) => {
   res.render("login");
